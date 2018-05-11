b440b0163603ffcc48e1fdfdf00abe15724051de
Joan Sebastian Chamorro <joan.chamorro1116@gmail.com>
2018-05-11 18:02:00 -0500

    first commit

--- app.py
+++ app.py
@@ -1,2 +1,22 @@
+import urllib.request
+import zipfile
 import click
+
+ 
+
 @click.command()
+@click.argument('download')
+def down(download):
+	click.echo('Downloading the dataset...')
+	response = urllib.request.urlretrieve("http://benchmark.ini.rub.de/Dataset_GTSDB/FullIJCNN2013.zip", "dataset.zip")
+	
+	with zipfile.ZipFile('dataset.zip',"r") as z:
+		z.extractall("E:\German-Traffic-Signs-Detector\images")
+
+
+
+#
+
+
+if __name__ == '__main__':
+	down()
